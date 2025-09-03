168bbee366e35d29de76d505e8fdaa7eb812cf02
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-09-03 11:17:51 +0530

    update file

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ const search = document.querySelector('.search');
             search.classList.toggle('active');
         });
 
-        document.querySelector(".option2").addEventListener('load', () => {
+        window.addEventListener('load', () => {
             document.querySelector('.select').classList.remove('btn')
         })
 
         document.querySelector(".option2").addEventListener('click', () => {
             document.querySelector('.select').classList.toggle('btn')
         })
+        
+        // document.querySelector('.line-mob').addEventListener('load',()=>{
+        //     document.querySelector('.second').classList.remove('mob-opt')
+        // })
+        // document.querySelector('.line-mob').addEventListener('click',()=>{
+        //     document.querySelector('.second').classList.toggle('mob-opt')
+        // })
